400375968ed81ab01163fae74810aea40ae201de
aweelbeast <avi.k.ftwjk@gmail.com>
2021-07-31 17:08:19 +0800

    zip_me edits for jul 31

--- test_zip.py
+++ test_zip.py
@@ -1,10 +1,39 @@
 import zipfile 
 import os
+import sys
+  
+from flask import Flask, render_template, request
+#from werkzeug import secure_filename
+app = Flask(__name__)
 
-zip = zipfile.ZipFile('test_file_2.zip', 'w')
-print(os.getcwd())
-for file in os.listdir(os.getcwd()):
-    if file.endswith(".txt"):
-        zip.write(os.path.join(os.getcwd(), file), file, compress_type = zipfile.ZIP_DEFLATED)
+@app.route('/form')
+def form():
+    return render_template('form.html')
+	
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(f.filename)
 
-zip.close()
+      # importing ZipFile class from zipfile module
+      from zipfile import ZipFile
+
+      # more fine-grained control over ZIP files
+      with ZipFile("zip_return.zip", "w") as newzip:
+          newzip.write(f.filename)
+          newzip.close()
+
+      return (
+          """\
+            <html>
+            <body>
+                <h1> TADA </h1>
+                <p> <a href=\"""" + os.getcwd() + """\"zip_return.zip\" download> Download </a> </p>
+            </body>
+            </html>
+          """
+      )
+		
+if __name__ == '__main__':
+   app.run(host='localhost', port=5000, debug=True)
