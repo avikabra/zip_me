d10f92660dcc93dd332c6dc10c051bdd43e71850
aweelbeast <avi.k.ftwjk@gmail.com>
2021-08-02 17:46:40 +0800

    zip website with flask

--- test_zip.py
+++ test_zip.py
@@ -2,9 +2,10 @@ import zipfile
 import os
 import sys
   
-from flask import Flask, render_template, request
-#from werkzeug import secure_filename
+from flask import Flask, render_template, request, current_app, send_from_directory
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 @app.route('/form')
 def form():
@@ -13,27 +14,29 @@ def form():
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
-      f.save(f.filename)
+      f = request.files.getlist("file")
+      for p in f: 
+        p.save('uploads/' + p.filename)
 
       # importing ZipFile class from zipfile module
       from zipfile import ZipFile
 
       # more fine-grained control over ZIP files
-      with ZipFile("zip_return.zip", "w") as newzip:
-          newzip.write(f.filename)
+      with ZipFile("uploads/zip_return.zip", "w") as newzip:
+          for p in f:
+            newzip.write('uploads/' + p.filename)
           newzip.close()
 
       return (
-          """\
-            <html>
-            <body>
-                <h1> TADA </h1>
-                <p> <a href=\"""" + os.getcwd() + """\"zip_return.zip\" download> Download </a> </p>
-            </body>
-            </html>
-          """
+        render_template('uploads.html')
       )
+
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    # Returning file from appended path
+    return send_from_directory(directory=uploads, path=filename)
+    
 		
 if __name__ == '__main__':
    app.run(host='localhost', port=5000, debug=True)
